27d92fe415744abe7787af059b3d38df1a12cc7b
sownyluv <snowyfe@proton.me>
2024-08-02 17:51:23 -0500

    api updates idk what to write

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"static-api/cmd/services/friends"
 	"static-api/cmd/services/messages"
+	"static-api/cmd/services/token"
 	"static-api/cmd/services/user"
 
 	"github.com/gorilla/mux"
@@ -23,12 +24,16 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
+	//this is probably a shitty way to register services
 	userService := user.NewHandler()
 	userService.RegisterRoutes(subrouter)
 	messageService := messages.NewHandler()
 	friendService := friends.NewHandler()
 	friendService.RegisterRoutes(subrouter)
 	messageService.RegisterRoutes(subrouter)
+	tokenService := token.NewHandler()
+	tokenService.RegisterRoutes(subrouter)
+	//
 	fmt.Println("Listening on http://localhost" + s.addr + "/api/v1/")
 	return http.ListenAndServe(s.addr, router)
 }
---
+++ cmd/services/token/route.go
@@ -0,0 +1,68 @@
+package token
+
+import (
+	"encoding/json"
+	"net/http"
+	"static-api/db"
+	"static-api/helpers/auth"
+
+	"github.com/gorilla/mux"
+)
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/token", h.GetUser).Methods("POST")
+}
+
+func (h *Handler) GetUser(w http.ResponseWriter, r *http.Request) {
+	conn := db.DB
+	claims, err := auth.GetSession(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	tokenCookie, err := r.Cookie("token")
+	if err != nil {
+		http.Error(w, "Token cookie not found", http.StatusBadRequest)
+		return
+	}
+	userID := claims.ID
+
+	rows, err := conn.Query("SELECT id, username, email FROM users WHERE id = $1", userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var id int
+	var username, email string
+	if rows.Next() {
+		err := rows.Scan(&id, &username, &email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+	response := map[string]interface{}{
+		"success": true,
+		"Token":   tokenCookie.Value,
+		"user": map[string]interface{}{
+			"id":       id,
+			"username": username,
+			"email":    email,
+		},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
